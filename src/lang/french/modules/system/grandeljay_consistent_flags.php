b6cc834bf2109bef0171f24c36a44d55c5707976
grandeljay <github.jay@grandel.anonaddy.me>
2022-12-27 13:06:14 +0100

    Add French translation

---
+++ src/lang/french/modules/system/grandeljay_consistent_flags.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Consistent Flags
+ *
+ * A modified-shop Module which changes all of the flags for each language to a consistent style.
+ *
+ * @author  Jay Trees <consistent-flags@grandels.email>
+ * @link    https://github.com/grandeljay/modified-consistent-flags
+ * @package GrandelJayConsistentFlags
+ */
+
+define('MODULE_GRANDELJAY_CONSISTENT_FLAGS_TITLE', 'Consistent Flags');
+define('MODULE_GRANDELJAY_CONSISTENT_FLAGS_LONG_DESCRIPTION', 'Remplace les drapeaux de tous les packs de langue par des drapeaux cohérents, avec le même style.');
+define('MODULE_GRANDELJAY_CONSISTENT_FLAGS_STATUS_TITLE', 'Activer le module ?');
+define('MODULE_GRANDELJAY_CONSISTENT_FLAGS_STATUS_DESC', 'Faut-il utiliser des drapeaux cohérents ?');

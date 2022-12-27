a68489c13c17b79f09e3ec9398b7147996310192
grandeljay <github.jay@grandel.anonaddy.me>
2022-12-27 13:06:22 +0100

    Add Italian translation

---
+++ src/lang/italian/modules/system/grandeljay_consistent_flags.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Consistent Flags
+ *
+ * A modified-shop Module which changes all of the flags for each language to a consistent style.
+ *
+ * @author  Jay Trees <consistent-flags@grandels.email>
+ * @link    https://github.com/grandeljay/modified-consistent-flags
+ * @package GrandelJayConsistentFlags
+ */
+
+define('MODULE_GRANDELJAY_CONSISTENT_FLAGS_TITLE', 'Consistent Flags');
+define('MODULE_GRANDELJAY_CONSISTENT_FLAGS_LONG_DESCRIPTION', 'Sostituisce le bandiere di tutti i pacchetti linguistici con bandiere coerenti, con lo stesso stile.');
+define('MODULE_GRANDELJAY_CONSISTENT_FLAGS_STATUS_TITLE', 'Attivare il modulo?');
+define('MODULE_GRANDELJAY_CONSISTENT_FLAGS_STATUS_DESC', 'Si devono usare bandiere coerenti?');

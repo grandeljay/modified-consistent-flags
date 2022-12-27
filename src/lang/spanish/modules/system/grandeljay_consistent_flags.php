f6a9112ee480b9aadde15f22ae48e455353a64c2
grandeljay <github.jay@grandel.anonaddy.me>
2022-12-27 13:06:31 +0100

    Add Spanish translation

---
+++ src/lang/spanish/modules/system/grandeljay_consistent_flags.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Consistent Flags
+ *
+ * A modified-shop Module which changes all of the flags for each language to a consistent style.
+ *
+ * @author  Jay Trees <consistent-flags@grandels.email>
+ * @link    https://github.com/grandeljay/modified-consistent-flags
+ * @package GrandelJayConsistentFlags
+ */
+
+define('MODULE_GRANDELJAY_CONSISTENT_FLAGS_TITLE', 'Consistent Flags');
+define('MODULE_GRANDELJAY_CONSISTENT_FLAGS_LONG_DESCRIPTION', 'Sustituye las banderas de todos los paquetes de idiomas por banderas coherentes, con el mismo estilo.');
+define('MODULE_GRANDELJAY_CONSISTENT_FLAGS_STATUS_TITLE', '¿Activar módulo?');
+define('MODULE_GRANDELJAY_CONSISTENT_FLAGS_STATUS_DESC', '¿Deben utilizarse banderas coherentes?');

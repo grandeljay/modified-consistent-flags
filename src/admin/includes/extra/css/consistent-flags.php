a4c016cbf765b72f9f28aedcf7ebc5cd313e5ac3
grandeljay <github.jay@grandel.anonaddy.me>
2023-01-06 14:06:42 +0100

    Abort if module is inactive

--- src/admin/includes/extra/css/consistent-flags.php
+++ src/admin/includes/extra/css/consistent-flags.php
@@ -8,6 +8,12 @@
  * @package GrandelJayConsistentFlags
  */
 
+$is_enabled = defined('MODULE_GRANDELJAY_CONSISTENT_FLAGS_STATUS') && 'true' === MODULE_GRANDELJAY_CONSISTENT_FLAGS_STATUS;
+
+if (false === $is_enabled) {
+    return;
+}
+
 $filename = 'includes/css/consistent-flags.css';
 $version  = hash_file('crc32c', DIR_FS_ADMIN . $filename);
 ?>

8740a78d31e63cfc6a6951ebef20d3278271b27c
grandeljay <github.jay@grandel.anonaddy.me>
2023-01-30 11:35:28 +0100

    Bump minimum requirements

--- src/admin/includes/modules/system/grandeljay_consistent_flags.php
+++ src/admin/includes/modules/system/grandeljay_consistent_flags.php
@@ -20,11 +20,7 @@ class grandeljay_consistent_flags extends StdModule
 
     public function __construct()
     {
-        if (function_exists('parent::__construct()')) {
-            parent::__construct();
-        } else {
-            $this->init('MODULE_' . strtoupper(self::class));
-        }
+        parent::__construct();
     }
 
     public function display()
